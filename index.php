027fdd74b2b0e31418943f75ae7c37cfd1c5fdbb
pamelass2022 <113396453+pamelass2022@users.noreply.github.com>
2022-11-05 12:24:16 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+	 <meta charset="UTF-8"> 
+    <head>
+	   
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Prédio MDPS</title>
+	   
+		
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/fontawesome.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Lobster|Lobster+Two|Noto+Serif" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    </head>
+    <body>
+        
+        <nav class="navbar navbar-expand-lg navbar-light bg-light text-capitalize main-font-family">
+            <div class="container">
+                <a class="navbar-brand" href="index.html"><img src="imgs/predio.gif" width=120 /></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#show-menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span> 
+                </button>
+
+                <div class="collapse navbar-collapse" id="show-menu"> 
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#home">Início <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#room">Quartos</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                          <!--  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Caracteristicas
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">link</a>
+                                <a class="dropdown-item" href="#">Another link</a>
+                                <a class="dropdown-item" href="#">Another link</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div> -->
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="#blog">Fotos do Apartamento</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">contatos</a>
+                        </li>
+                         <class="nav-item book d-flex align-items-center">
+                          <!--  <button> <a class="nav-link" href="agendamento.html">agende agora</a> </button> 
+						  <button><a href="agendamento.html">Agende Agora</a></button> -->
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <header id="home">
+            <div class="small-container">
+                <form class="main-font-family text-center">
+                <!--    <input type="search"> -->
+                <!--    <i class="fas fa-search"></i> -->
+                <!--   <input type="submit" value="Search"> -->
+                </form>
+                <div class="row">
+                    <div class="col-lg-4 col-12 lobster-font-family d-flex align-items-center">
+                        <h2> <font color=#DEEFE7> Colecione bons momentos com a gente!</h2> </font>
+                      <!--  <button><a href="#">sobre o prédio</a></button> -->
+                    </div>
+                </div>
+                <div class="h-slider roboto-font-family welcome d-flex align-items-center justify-content-center"> 
+                    <h1 class="text-capitalize">Bem-Vindos! <br><span><b>Prédio MDPS</b></span></h1>
+                  <!-- <div id="headerslider" class="carousel slide" data-ride="carousel"> -->
+                    <!--  <div class="carousel-inner"> -->
+                      <!--  <div class="carousel-item carousel-three active"></div> -->
+                      <!--  <div class="carousel-item carousel-two"></div> -->
+                      <!--  <div class="carousel-item carousel-one"></div> -->
+                   <!--   </div> -->
+                     <!-- <a class="carousel-control-prev" href="#headerslider" role="button" data-slide="prev"> -->
+                       <!-- <i class="fas fa-angle-double-left"></i>  -->
+                        <span class="sr-only">Previous</span>
+                      </a>
+                   <!--   <a class="carousel-control-next" href="#headerslider" role="button" data-slide="next"> -->
+                       <!-- <i class="fas fa-angle-double-right"></i>  -->
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </div>
+                </div>
+            </div>
+            <div class="st-rec"></div>
+            <div class="rd-rec"></div>
+        </header>
+        <br> <br>
+        <div class="about lobster-font-family">
+            <div class="container">
+                <h2 class="text-center text-capitalize">Sobre O Nosso Prédio</h2>
+				<img src="imgs/shape.png">
+				<table width=150%>
+				 <tr>
+                
+			   <td>	<img src=imgs/predio.png width=300>  </td>
+				
+               <td> <div class="row">
+                    <div class="col-lg-6 col-12">
+                        <h4>O melhor lugar para curtir sua vida</h4> 
+                        <p>Todo mundo precisa de um lugar para recarregar as baterias. <br> 
+						O MDPS é uma excelente opção para descansar e se renovar.<br> 
+						Conhecido por seu ambiente familiar e pela proximidade de ótimos restaurantes e de atrações famosas,<br> 
+						o MDPS faz com que seja fácil desfrutar o melhor de Campos dos Goytacazes. </p>
+                        <button><a href="informacoes.html">Consulte mais informações</a></button>
+						
+                    </div> 
+                    <div class="col-lg-6 col-12">
+                        <div class="img"></div>
+                    </div> </td>
+					
+					</tr>
+					</table width=150%>
+					<tr>
+				<br> <br> <br>
+                </div>
+				
+             <center>   <h2 class="text-capitalize" id="room">Quarto e Termos</h2> </center>
+				<table>
+                <div class="row">
+				<td><img src=imgs/quarto3.jpg width=500></td>
+                 <td>   <div class="col-lg-4 col-12">
+                        <div class="img"></div>
+                    </div>
+                    <div class="col-lg-8 col-12">
+                        <div class="block">
+						
+                            <div>
+                                
+                                <strong>$400.000,00 Compra</strong>
+                                
+                                <p>Ótimos apartamentos com 2 dormitorios,
+                                   com sala espaçosa, cozinha,
+								   com armários, Área de serviço e
+								   com duas vagas de garagem.
+                          		</p>
+                               <button><a href="cadastro.php" class="text-capitalize">Cadastro</a></button> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+		</tr> </td>
+    </table>    
+	
+	<br> <br>
+        <div class="gallery lobster-font-family" id="blog">
+            <div class="container">
+              <center>  <h2 class="text-calitalize text-center">Fotos do Apartamento</h2> </center>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="pic-one"><h4>banheiro<img src=imgs/banheiro.jpg></h4></div>
+						
+                        <div class="pic-two"><h4>Cozinha<img src=imgs/cozinha.jpg width=200%></h4></div>
+						
+					
+                    </div>
+					
+                    <div class="col-lg-4 col-md-6 col-12">
+					
+                        <div class="pic-three active"><h4>Sala<img src=imgs/sala.jpg></h4></div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="pic-four"><h4>Quarto 1<img src=imgs/quarto1.jpg></h4></div>
+                        <div class="pic-five"><h4>Quarto 2<img src=imgs/quarto2.jpg width=200%></</h4></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="slider main-font-family">
+            <h2 class="text-center text-capitalize main-font-family">O Que Nossos Clientes Dizem</h2>
+          <div id="slideToNext" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#slideToNext" data-slide-to="0" class="active"></li>
+              <li data-target="#slideToNext" data-slide-to="1"></li>
+              <li data-target="#slideToNext" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="carousel-caption d-block">
+                    <img src=imgs/senhor.jpg>
+                  <h5>Romeu</h5>
+                  <p> Um lugar muito acolhedor, adorei o espaço e os movéis. </p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="carousel-caption d-block">
+                    <img src="imgs/senhora.jpg">
+                  <h5>Maria</h5>
+                  <p> Adorei o prédio! A cozinha é linda o quarto é maravilhoso. </p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="carousel-caption d-block">
+                    <img src="imgs/clara.jpg">
+                  <h5>Clara</h5>
+                  <p> Maravilhoso! 4 estrelas...</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br> <br> <br>
+		
+        <div class="contact main-font-family text-center">
+            <div class="container">
+               <center> <h2 id="contact">Entrar em contato</h2> </center>
+                <div class="row"> 
+				<center> 
+                    <div class="col-6">
+                      <div class="contact-form"> 
+                           <form method=POST>
+							     Nome:
+                                <input type="text" name=nome> 
+								Email:
+                                <input type="text" name=email>
+								Telefone:
+                                <input type="text" name=telefone>
+								Mensagem:
+                                <textarea name=mensagem> </textarea>
+                                <input type="submit" value="enviar"> 
+                            </form>
+                         </div> 
+                 </div>    
+                 </center>   
+				 <div class="col-6">
+                        
+                    </div>
+                </div>
+            </div> 
+            <div></div>
+        </div> 
+        
+        <footer class="noto-font-family">
+            <div class="overlay">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 col-12">
+						<img src="imgs/Prédio.gif" width=300 />
+                          <!--  <h3>links</h3>
+                            <ul class="text-capitalize">
+                                <li><a href="#">Casa</a></li>
+                                <li><a href="#">quartos</a></li>
+                                <li><a href="#">Rooms</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Contacts</a></li> #20B2AA
+                            </ul> -->
+                        </div>
+						
+                      <div class="col-lg-4 col-md-6 col-12">
+		 			  
+                            <h3>Endereço:</h3>
+                                Avenida Caminho do Sonho, 200,  Campos Dos Goytacazes/RJ
+
+                                <br>
+                                +55 22 98786546<br>
+                                mdps@gmail.com
+							
+                        </div>
+						
+                        <div class="col-lg-4 col-md-6 col-12 form">
+                           <!-- <h3>Fale conosco</h3>
+                            <form>
+                                <input type="text" name=email>
+                                <input type="submit">
+                            </form> -->
+							Entre em contato conosco pelo whatsapp:
+                            <ul>
+                               <!-- <li><a href="#"><i class="fab fa-facebook-f"></i></a></li> -->
+                                <li><a href="https://chat.whatsapp.com/IQQb7OshR2qDHOiRvDJztL"><i class="fab fa-whatsapp"></i></a></li>
+                              <!--  <li><a href="#"><i class="fab fa-instagram"></i></a></li> -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
+      <!--  <div class="copyright noto-font-family">
+            <p>Â© 2019 All Rights Reserved. Design by <a href="https://html.design/">Free Html Templates</a></p>
+        </div>
+        -->
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script>
+            $(function () {
+                
+                'use strict';
+                
+                var winH = $(window).height();
+                
+                $('header').height(winH);
+                
+                $('.navbar ul.navbar-nav li a').on('click', function (e) {
+                    
+                    var getAttr = $(this).attr('href');
+                    
+                    e.preventDefault();
+                    $('html').animate({scrollTop: $(getAttr).offset().top}, 1000);
+                });
+            });
+        </script>
+    </body>
+</html>
+
+<?php
+if(isset($_POST["nome"])){
+$conexao=mysql_connect("localhost:3306","root", "root");
+mysql_select_db("predio",$conexao);
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$telefone = $_POST["telefone"];
+$mensagem = $_POST["mensagem"];
+mysql_query("insert into contato values('$nome','$email','$telefone','$mensagem')",$conexao);
+$consulta = mysql_query("select * from contato",$conexao);
+while($_GET = mysql_fetch_array($consulta)){
+echo "<br>Nome:".$_GET['nome'];
+echo " - Email:".$_GET['email'];
+echo " - Telefone:".$_GET['telefone'];
+echo " - Mensagem:".$_GET['mensagem'];
+}
+mysql_close($conexao);
+}
+?>
