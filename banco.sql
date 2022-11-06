f37307d68dc646084090b6ece0e749c96786a8e4
pamelass2022 <113396453+pamelass2022@users.noreply.github.com>
2022-11-06 12:43:32 -0300

    Add files via upload

---
+++ banco.sql
@@ -0,0 +1,13 @@
+create database mdps;
+
+CREATE TABLE mdps.contato (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telefone INT(14) NOT NULL,
+	mensagem INT(255) NOT NULL
+);
+
+create user 'mdps'@'%' identified by 'mdps123';
+
+grant all privileges on mdps.* to 'mdps'@'%';
